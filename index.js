febaadb7b5094e0aafe17e0ef079565cb693db6f
aqwertyuiop48 <aqwertyuiop48@outlook.com>
2026-03-14 17:46:02 +0530

    Update internal URLs

--- index.js
+++ index.js
@@ -2247,7 +2247,7 @@ server.route({
       };
       sprite = new Image();
       sprite.src =
-        "https://image.ibb.co/dr1HZy/Pf_RWr3_X_Imgur.png" +
+        "https://i.imgur.com/PfRWr3X.png" +
         "?" +
         new Date().getTime();
       sprite.setAttribute("crossOrigin", " ");
@@ -2259,7 +2259,7 @@ server.route({
       };
     
       finishSprite = new Image();
-      finishSprite.src = "https://image.ibb.co/b9wqnJ/i_Q7m_U25_Imgur.png"+
+      finishSprite.src = "https://i.imgur.com/iQ7mU25.png"+
       "?" +
       new Date().getTime();
       finishSprite.setAttribute("crossOrigin", " ");
